aab655ba9ca995a284859fc696feb976d59b8069
itw_zhangqh <zhqh9016@163.com>
2017-03-04 17:50:02 +0800

    添加更简洁的实现递归归并排序实现

--- src/com/zhqh/sort/merge/MergeTwoSortedArray.java
+++ src/com/zhqh/sort/merge/MergeTwoSortedArray.java
@@ -1,5 +1,7 @@
 package com.zhqh.sort.merge;
 
+import org.junit.Test;
+
 /**
  * 将两个有序的数组合并为一个有序的数组 该方法是为后续使用归并算法进行排序做准备的
  * 
@@ -10,7 +12,7 @@ public class MergeTwoSortedArray {
 
 	public static void main(String[] args) {
 		int[] sourceA = { 1, 3, 5, 7, 9 };
-		int[] sourceB = { 2, 4, 6, 8, 10,11 };
+		int[] sourceB = { 2, 4, 6, 8, 10, 11 };
 		int[] merge = merge(sourceA, sourceB);
 		for (int i : merge) {
 			System.out.println(i);
@@ -55,6 +57,60 @@ public class MergeTwoSortedArray {
 			}
 		}
 		return result;
+	}
+
+	@Test
+	public void testMergeCopyFromInternet() {
+		int[] sourceA = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10, 11 };
+		mergeCopyFromInternet(sourceA, 2, 4, 7);
+		for (int i : sourceA) {
+			System.out.println(i);
+		}
+	}
+
+	/**
+	 * 从网上查找的实现将两个有序数组合并为一个有序数组
+	 * 
+	 * @param source
+	 * @param begin
+	 * @param middle
+	 * @param end
+	 */
+	public static void mergeCopyFromInternet(int[] source, int begin, int middle, int end) {
+
+		// 计算两个数组的长度
+		int len1 = middle - begin + 1;
+		int len2 = end - middle;
+
+		// 声明两个临时数组用于存放需要被整合的源数组
+		//为了避免对角标越界的判断,这里分配空间时多分配一个标识边界的元素
+		int[] temp1 = new int[len1 + 1];
+		temp1[len1] = Integer.MAX_VALUE;
+		int[] temp2 = new int[len2 + 1];
+		temp2[len2] = Integer.MAX_VALUE;
+		
+
+		for (int i = 0; i < len1; i++) {
+			temp1[i] = source[begin + i];
+		}
+
+		for (int i = 0; i < len2; i++) {
+			temp2[i] = source[middle + i + 1];
+		}
+
+		int a = 0;
+		int b = 0;
+		// 将两个有序数组合并为一个有序数组
+		for (int k = begin; k <= end; k++) {
+			if (temp1[a] < temp2[b]) {
+				source[k] = temp1[a];
+				a++;
+			} else {
+				source[k] = temp2[b];
+				b++;
+			}
+		}
+
 	}
 
 }
--- src/com/zhqh/sort/merge/RecursionMergeSort.java
+++ src/com/zhqh/sort/merge/RecursionMergeSort.java
@@ -2,6 +2,8 @@ package com.zhqh.sort.merge;
 
 import java.util.Random;
 
+import org.junit.Test;
+
 /**
  * 使用递归方式实现归并排序
  * 
@@ -9,9 +11,9 @@ import java.util.Random;
  *
  */
 public class RecursionMergeSort {
-	
+
 	public static void main(String[] args) {
-		
+
 		int seed = 2000000;
 
 		// int[] source = {8,9,37,40,8,12,83,6,48,7,26,34,72,3,48,7};
@@ -21,34 +23,35 @@ public class RecursionMergeSort {
 			int nextInt = random.nextInt(seed);
 			source[i] = nextInt;
 		}
-		
+
 		long start = System.currentTimeMillis();
 		int[] mergeSort = mergeSort(source);
 		long end = System.currentTimeMillis();
-		
-		for(int j=1;j<mergeSort.length;j++){
-			if(mergeSort[j-1]>mergeSort[j]){
+
+		for (int j = 1; j < mergeSort.length; j++) {
+			if (mergeSort[j - 1] > mergeSort[j]) {
 				System.out.println("排序结果不正确");
 				return;
 			}
 		}
 		System.out.println("对" + seed + "个数进行排序用时" + (end - start) + "ms");
-		
+
 	}
 
 	/**
 	 * 归并排序实现
+	 * 
 	 * @param source
 	 * @return
 	 */
 	public static int[] mergeSort(int[] source) {
 
-		//如果当前数组只有一个元素,那么不需要进行排序直接将数组返回
+		// 如果当前数组只有一个元素,那么不需要进行排序直接将数组返回
 		int sourceLen = source.length;
 		if (sourceLen <= 1) {
 			return source;
 		}
-		//将源数组从中间拆分成两个数组
+		// 将源数组从中间拆分成两个数组
 		int mid = sourceLen / 2;
 		int[] sourceA = new int[mid];
 		int[] sourceB = new int[sourceLen - mid];
@@ -59,14 +62,55 @@ public class RecursionMergeSort {
 		for (int j = 0; j < sourceLen - mid; j++) {
 			sourceB[j] = source[mid + j];
 		}
-		
-		//递归调用对拆分后的两个数组进行归并排序
+
+		// 递归调用对拆分后的两个数组进行归并排序
 		int[] mergeSortLeft = mergeSort(sourceA);
 		int[] mergeSortRigth = mergeSort(sourceB);
-		//拆分后的数组排序完成后合并为一个有序数组
+		// 拆分后的数组排序完成后合并为一个有序数组
 		int[] merge = MergeTwoSortedArray.merge(mergeSortLeft, mergeSortRigth);
 
 		return merge;
 	}
 
+	@Test
+	public void testMergeSortCopyFromInternet() {
+		int seed = 20000000;
+
+		int[] source = new int[seed];
+		Random random = new Random();
+		for (int i = 0; i < seed; i++) {
+			int nextInt = random.nextInt(seed);
+			source[i] = nextInt;
+		}
+
+		long start = System.currentTimeMillis();
+		mergeSortCopyFromInternet(source, 0, seed - 1);
+		long end = System.currentTimeMillis();
+
+		for (int j = 1; j < source.length; j++) {
+			if (source[j - 1] > source[j]) {
+				System.out.println("排序结果不正确");
+				return;
+			}
+		}
+
+		System.out.println("对" + seed + "个数进行排序用时" + (end - start) + "ms");
+	}
+
+	/**
+	 * 从网上查找的递归归并实现
+	 * 
+	 * @param source
+	 * @param begin
+	 * @param end
+	 */
+	public static void mergeSortCopyFromInternet(int[] source, int begin, int end) {
+
+		int middle = (end + begin) / 2;
+		if (begin < end) {
+			mergeSortCopyFromInternet(source, begin, middle);
+			mergeSortCopyFromInternet(source, middle + 1, end);
+			MergeTwoSortedArray.mergeCopyFromInternet(source, begin, middle, end);
+		}
+	}
 }
